70c2a6e9424900aaad6453f49f7d4048ca18bcd5
Carla Estrada <cferestrada@gmail.com>
2017-09-03 19:06:35 +0000

    first commit

---
+++ run.c
@@ -0,0 +1,3 @@
+#!/bin/sh
+gcc sum.c -out sum
+./sum
---
+++ sum.c
@@ -0,0 +1,13 @@
+#include <stdlib.h> //Standard library
+#include <stdio.h> //Standard input / output
+
+int main()
+{
+    int a=0,b=0,sum=0;    	
+    
+    
+    scanf("%d%d",&a,&b);       
+    sum=a+b;                  
+    printf("%d\n", sum);      
+    return 0;
+}
